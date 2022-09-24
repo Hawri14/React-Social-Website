9af123bf782d8b1e928c6534ccada44b0e892240
Hawri <hawrikazaz@gmail.com>
2022-09-24 21:08:56 +0100

    json commit

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,9 +1,25 @@
 import PostMessage from "../models/postMessage.js";
 
-export const getPost = (req, res) => {
-    res.send('THIS WORKS!');
+export const getPosts =async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 }
 
-export const createPost = (req, res) => {
-    res.send('Post Creation');
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+
+    try {
+        await newPost.save();
+//this means that the post has been successful
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).jason({ message: error.message }); //means that it was not successful
+    }
 }
