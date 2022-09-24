98fe6292a014992d2875876705146624b5fb8dd6
Hawri <hawrikazaz@gmail.com>
2022-09-24 20:33:26 +0100

    added routers

---
+++ server/controllers/posts.js
@@ -0,0 +1,9 @@
+import PostMessage from "../models/postMessage.js";
+
+export const getPost = (req, res) => {
+    res.send('THIS WORKS!');
+}
+
+export const createPost = (req, res) => {
+    res.send('Post Creation');
+}
