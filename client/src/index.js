7ba1c36a6d6b5d51bdb829d07241cec40812b00a
Hawri <hawrikazaz@gmail.com>
2022-09-24 19:09:26 +0100

    initial project setup

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+//here we are connecting to the div with the ID "root"
+ReactDOM.render(<App />, document.getElementById('root'));
