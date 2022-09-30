a6c6b28a5b6704151238f7bc42059d769c3ea651
Hawri <hawrikazaz@gmail.com>
2022-10-01 00:41:43 +0100

    added likes and added functionality

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose, { mongo } from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 
 
@@ -15,10 +16,9 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 //this is the database cluster we are using from mongoDB atlas
-const CONNECTION_URL = 'mongodb+srv://React1:React1123@cluster0.etx85rt.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
-//connect the server to localhost 5000 and make it listen to the port
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//connect the server to localhost 5000 and make it listen to the port, use the dovenv file to hid information from users
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
